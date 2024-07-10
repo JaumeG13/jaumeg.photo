27f7d9c58fdc9485d450f2e768b59c317b8b0dbe
JaumeG13 <jaumegenaro02@gmail.com>
2024-07-10 14:06:46 +0200

    progressing

--- assets/js/language.js
+++ assets/js/language.js
@@ -1,18 +1,18 @@
 function setDefault() {
-    document.getElementById('en').style.display = 'none';
-    document.getElementById('es').style.display = 'none';
+    document.getElementById('eng').style.display = 'none';
+    document.getElementById('esp').style.display = 'none';
 }
 
 function toggleLanguage() {
-    if(document.getElementById('switch').checked) {
+    if(document.getElementById('switch').checked = true) {
         setDefault();
-        document.getElementById('en').style.display = 'none';
-        document.getElementById('es').style.display = 'block';
+        document.getElementById('eng').style.display = 'none';
+        document.getElementById('esp').style.display = 'block';
     }
 
     if(document.getElementById('switch').checked = false) {
         setDefault();
-        document.getElementById('en').style.display = 'block';
-        document.getElementById('es').style.display = 'none';
+        document.getElementById('eng').style.display = 'block';
+        document.getElementById('esp').style.display = 'none';
     }
 }
