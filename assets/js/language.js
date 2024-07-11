4160e186035caccaf61399054d12b74828e47484
JaumeG13 <jaumegenaro02@gmail.com>
2024-07-11 12:53:34 +0200

    es-en button

--- assets/js/language.js
+++ assets/js/language.js
@@ -1,3 +1,4 @@
+/*
 var checkbox = document.getElementById('switch');
 var espanol = document.getElementsByClassName('esp');
 var english = document.getElementsByClassName('eng');
@@ -14,6 +15,36 @@ function toggleLanguage() {
     } else {
         setDefault();
         espanol.style.display = 'none';
+    }
+}
+
+checkbox.addEventListener('change', toggleLanguage);
+*/
+
+var checkbox = document.getElementById('switch');
+var espanol = document.getElementsByClassName('esp');
+var english = document.getElementsByClassName('eng');
+
+function setDefault() {
+    for (var i = 0; i < english.length; i++) {
+        english[i].style.display = 'block';
+    }
+    for (var i = 0; i < espanol.length; i++) {
+        espanol[i].style.display = 'block';
+    }
+}
+
+function toggleLanguage() {
+    setDefault(); // Call setDefault first to reset all elements
+
+    if (checkbox.checked === true) {
+        for (var i = 0; i < english.length; i++) {
+            english[i].style.display = 'none';
+        }
+    } else {
+        for (var i = 0; i < espanol.length; i++) {
+            espanol[i].style.display = 'none';
+        }
     }
 }
 
